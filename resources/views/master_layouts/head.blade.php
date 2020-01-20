4725b0fd2c6d9fa4ff80a96c9561386856dc90c5
TarunShrivastva <shrivastvatarun@gmail.com>
2020-01-20 16:22:27 +0530

    changes for frontend by tarun on 04:21 20-01-2020

---
+++ resources/views/master_layouts/head.blade.php
@@ -0,0 +1,45 @@
+<head>
+<title>Restaurant</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="{{ URL::to('css/reset.css') }}" type="text/css" media="all">
+<link rel="stylesheet" href="{{ URL::to('css/layout.css') }}" type="text/css" media="all">
+<link rel="stylesheet" href="{{ URL::to('css/style.css') }}" type="text/css" media="all">
+<link rel="stylesheet" href="{{ URL::to('css/zerogrid.css') }}" type="text/css" media="all">
+<link rel="stylesheet" href="{{ URL::to('css/responsive.css') }}" type="text/css" media="all"> 
+<link rel="stylesheet" href="{{ URL::to('css/responsiveslides.css') }}" /> 
+<script type="text/javascript" src="{{ URL::to('js/jquery-1.6.js') }}" ></script>
+<script type="text/javascript" src="{{ URL::to('js/cufon-yui.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('js/cufon-replace.js') }}"></script>  
+<script type="text/javascript" src="{{ URL::to('js/Forum_400.font.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('js/jquery.easing.1.3.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('js/tms-0.3.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('js/tms_presets.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('js/script.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('js/atooltip.jquery.js') }}"></script> 
+<script type="text/javascript" src="{{ URL::to('js/css3-mediaqueries.js') }}"></script>
+<script src="{{ URL::to('js/responsiveslides.js') }}"></script>
+<script>
+	$(function () {
+	  $("#slidez").responsiveSlides({
+		auto: true,
+		pager: false,
+		nav: true,
+		speed: 500,
+		maxwidth: 960,
+		namespace: "centered-btns"
+	  });
+	});
+</script>
+<!--[if lt IE 9]>
+	<script type="text/javascript" src="js/html5.js"></script>
+	<style type="text/css">
+		.slider_bg {behavior:url(js/PIE.htc)}
+	</style>
+<![endif]-->
+<!--[if lt IE 7]>
+	<div style='clear:both;text-align:center;position:relative'>
+		<a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" alt="" /></a>
+	</div>
+<![endif]-->
+</head>
---
+++ resources/views/master_layouts/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('master_layouts.head')
+<body id="page1">
+<div class="body6">
+	<div class="body1">
+		<div class="body5">
+			<div class="main zerogrid">
+			<!-- header -->
+				@include('master_layouts.header')
+			<!-- / header -->
+			<!-- content -->
+				@include('master_layouts.content')
+			<!-- / content -->
+			</div>
+		</div>
+	</div>
+</div>		
+<div class="body3">
+	<div class="main zerogrid">
+<!-- footer -->
+		@include('master_layouts.footer')
+<!-- / footer -->
+	</div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
